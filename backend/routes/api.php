20c0cb9d7b4c2ee150317da49b620b5a89ac227e
furugenmotoshi <amanogawa_m@icloud.com>
2021-10-23 03:02:05 +0900

    routeの書き方変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\API\WorkspaceController;
+use App\Http\Controllers\API\WorkspaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('workspace', 'App\Http\Controllers\API\WorkspaceController@index');
+Route::get('workspace', [WorkspaceController::class, 'index']);
