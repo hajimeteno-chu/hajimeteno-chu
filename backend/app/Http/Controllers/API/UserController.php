19dcf70cb83ae5c6492fc4925e4966925d06ac16
furugenmotoshi <amanogawa_m@icloud.com>
2021-10-23 19:11:30 +0900

    userへgetでそのユーザーを返す

--- backend/app/Http/Controllers/API/UserController.php
+++ backend/app/Http/Controllers/API/UserController.php
@@ -10,60 +10,6 @@ use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     public function login(Request $request)
     {
@@ -94,5 +40,13 @@ class UserController extends Controller
         }else{
             abort(401);
         }
+    }
+
+    private function resConversionJson($result, $statusCode=200)
+    {
+        if(empty($statusCode) || $statusCode < 100 || $statusCode >= 600){
+            $statusCode = 500;
+        }
+        return response()->json($result, $statusCode, ['Content-Type' => 'application/json'], JSON_UNESCAPED_SLASHES);
     }
 }
